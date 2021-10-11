244a2f549482b657924c1c78dc86b01d3a73a3a3
nicholasyqj@gmail.com <nicholasyqj@gmail.com>
2021-10-11 22:37:41 +0800

    supply chain exercise

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -4,26 +4,41 @@ pragma solidity >=0.5.16 <0.9.0;
 contract SupplyChain {
 
   // <owner>
+  address public owner;
 
   // <skuCount>
+  uint public skuCount;
 
   // <items mapping>
+  mapping (uint => Item) items;
 
   // <enum State: ForSale, Sold, Shipped, Received>
-
+  enum State{ ForSale, Sold, Shipped, Received}
   // <struct Item: name, sku, price, state, seller, and buyer>
+  struct Item {
+    string name;
+    uint sku;
+    uint price;
+    State state;
+    address payable seller;
+    address payable buyer;
+  }
   
   /* 
    * Events
    */
 
   // <LogForSale event: sku arg>
+  event LogForSale(uint sku);
 
   // <LogSold event: sku arg>
+  event LogSold(uint sku);
 
   // <LogShipped event: sku arg>
+  event LogShipped(uint sku);
 
   // <LogReceived event: sku arg>
+  event LogReceived(uint sku);
 
 
   /* 
@@ -31,25 +46,28 @@ contract SupplyChain {
    */
 
   // Create a modifer, `isOwner` that checks if the msg.sender is the owner of the contract
-
+  modifier isOwner(){
+    require(owner == msg.sender);
+    _;
+  }
   // <modifier: isOwner
 
   modifier verifyCaller (address _address) { 
-    // require (msg.sender == _address); 
+    require (msg.sender == _address); 
     _;
   }
 
   modifier paidEnough(uint _price) { 
-    // require(msg.value >= _price); 
+    require(msg.value >= _price); 
     _;
   }
 
   modifier checkValue(uint _sku) {
     //refund them after pay for item (why it is before, _ checks for logic before func)
     _;
-    // uint _price = items[_sku].price;
-    // uint amountToRefund = msg.value - _price;
-    // items[_sku].buyer.transfer(amountToRefund);
+    uint _price = items[_sku].price;
+    uint amountToRefund = msg.value - _price;
+    payable(items[_sku].buyer).transfer(amountToRefund);
   }
 
   // For each of the following modifiers, use what you learned about modifiers
@@ -61,16 +79,49 @@ contract SupplyChain {
   // an Item has been added?
 
   // modifier forSale
-  // modifier sold(uint _sku) 
-  // modifier shipped(uint _sku) 
-  // modifier received(uint _sku) 
+  modifier forSale(uint _sku){
+    require(items[_sku].price != 0, "Item does not exist yet");
+    require(items[_sku].state == State.ForSale, "Item is not for sale");
+    _;
+  }
+  modifier sold(uint _sku){
+    require(items[_sku].price != 0, "Item does not exist yet");
+    require(items[_sku].state == State.Sold, "Item is not Sold");
+    _;
+  }
+  modifier shipped(uint _sku) {
+    require(items[_sku].price != 0, "Item does not exist yet");
+    require(items[_sku].state == State.Shipped, "Item is not shipped");
+    _;
+  }
+  modifier received(uint _sku) {
+    require(items[_sku].price != 0, "Item does not exist yet");
+    require(items[_sku].state == State.Received, "Item is not received");
+    _;
+  }
 
   constructor() public {
+    owner = msg.sender;
+    skuCount = 0;
     // 1. Set the owner to the transaction sender
     // 2. Initialize the sku count to 0. Question, is this necessary?
   }
 
   function addItem(string memory _name, uint _price) public returns (bool) {
+
+    items[skuCount] = Item({
+      name: _name,
+      sku: skuCount,
+      price: _price,
+      state : State.ForSale,
+      seller : payable(msg.sender),
+      buyer : payable(address(0))
+    });
+
+    emit LogForSale(skuCount);
+    skuCount += 1;
+    return true;
+    
     // 1. Create a new item and put in array
     // 2. Increment the skuCount by one
     // 3. Emit the appropriate event
@@ -102,32 +153,44 @@ contract SupplyChain {
   //    - check the value after the function is called to make 
   //      sure the buyer is refunded any excess ether sent. 
   // 6. call the event associated with this function!
-  function buyItem(uint sku) public {}
+  function buyItem(uint sku) public forSale(sku) paidEnough(sku) checkValue(sku) payable {
+    items[sku].seller.transfer(items[sku].price);
+    items[sku].state = State.Sold;
+    items[sku].buyer = payable(msg.sender);
+    emit LogSold(sku);
+
+  }
 
   // 1. Add modifiers to check:
   //    - the item is sold already 
   //    - the person calling this function is the seller. 
   // 2. Change the state of the item to shipped. 
   // 3. call the event associated with this function!
-  function shipItem(uint sku) public {}
+  function shipItem(uint sku) sold(sku) verifyCaller(items[sku].seller) public {
+    items[sku].state = State.Shipped;
+    emit LogShipped(sku);
+  }
 
   // 1. Add modifiers to check 
   //    - the item is shipped already 
   //    - the person calling this function is the buyer. 
   // 2. Change the state of the item to received. 
   // 3. Call the event associated with this function!
-  function receiveItem(uint sku) public {}
+  function receiveItem(uint sku) shipped(sku) verifyCaller(items[sku].buyer) public {
+    items[sku].state = State.Received;
+    emit LogReceived(sku);
+  }
 
   // Uncomment the following code block. it is needed to run tests
-  /* function fetchItem(uint _sku) public view */ 
-  /*   returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) */ 
-  /* { */
-  /*   name = items[_sku].name; */
-  /*   sku = items[_sku].sku; */
-  /*   price = items[_sku].price; */
-  /*   state = uint(items[_sku].state); */
-  /*   seller = items[_sku].seller; */
-  /*   buyer = items[_sku].buyer; */
-  /*   return (name, sku, price, state, seller, buyer); */
-  /* } */
+   function fetchItem(uint _sku) public view 
+     returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) 
+   {      
+     name = items[_sku].name;
+     sku = items[_sku].sku;
+     price = items[_sku].price;
+     state = uint(items[_sku].state);
+     seller = items[_sku].seller;
+     buyer = items[_sku].buyer;
+     return (name, sku, price, state, seller, buyer);
+   } 
 }
--- truffle-config.js
+++ truffle-config.js
@@ -2,8 +2,13 @@ module.exports = {
   networks: {
     local: {
       host: "localhost",
-      port: 8545,
+      port: 7545,
       network_id: "*" // Match any network id
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.8.0"
     }
   }
 };
